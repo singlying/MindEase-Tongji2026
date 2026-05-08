e8ea87a89c3555872f03d7e23d1bf8f9e17862da
zuoranduoruo <2250832@tongji.edu.cn>
2026-05-08 14:00:00 +0800

    feat(admin): 迁移管理员量表管理界面

--- Backend/src/main/java/com/mindease/common/constant/JwtClaimsConstant.java
+++
@@ -1,9 +0,0 @@
-package com.mindease.common.constant;
-
-public class JwtClaimsConstant {
-
-    public static final String USER_ID = "userId";
-    public static final String USERNAME = "username";
-    public static final String ROLE = "role";
-}
-
